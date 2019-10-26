be360e500bf8ada19ad8b2ef7694ffe0a314185f
patrik <juandhdsrgmail.com>
2019-10-26 16:00:16 -0500

    clase 10

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const HelloWolrd = () =>{
+    <h1>Hola Mundo</h1>
+}
+
+export default HelloWolrd
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import HelloWorld from "module./components/HelloWorld.jsx";
+
+//render recibe dos inputs
+ReactDom.render(<HelloWorld/>, document.getElementById(app))
