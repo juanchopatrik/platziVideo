d58b167a5d785205bad81415a8f21dec88985655
patrik <juandhdsrgmail.com>
2019-10-29 11:40:35 -0500

    clase 20

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -11,7 +11,25 @@ const App = () => (
   <div className='App'>
     <Header />
     <Search />
-    <Categories>
+    <Categories title='Mi lista'>
+      <Carousel>
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+      </Carousel>
+    </Categories>
+    <Categories title='Tendencias'>
+      <Carousel>
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+      </Carousel>
+    </Categories>
+    <Categories title='Originales de Platfix'>
       <Carousel>
         <CarouselItem />
         <CarouselItem />
